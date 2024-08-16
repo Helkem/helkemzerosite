cd3592a91f20b49df3e58b1a4662206956096478
helkem0 <helkem0@protonmail.com>
2024-08-16 23:23:52 +0800

    Added animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { Badge } from "@/components/ui/badge";
 import { DATA } from "@/data/resume";
 import Link from "next/link";
 import Markdown from "react-markdown";
+import HyperText from "@/components/magicui/hyper-text";
 
 const BLUR_FADE_DELAY = 0.04;
 
@@ -19,11 +20,9 @@ export default function Page() {
         <div className='mx-auto w-full max-w-2xl space-y-8'>
           <div className='gap-2 flex justify-between'>
             <div className='flex-col flex flex-1 space-y-1.5'>
-              <BlurFadeText
-                delay={BLUR_FADE_DELAY}
-                className='text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none'
-                yOffset={8}
-                text={`${DATA.name.split(" ")[0]} `}
+              <HyperText
+                text='Helkem'
+                className='text-5xl font-semibold font-inter text-transform: capitalize'
               />
               <BlurFadeText
                 className='max-w-[600px] md:text-xl'
