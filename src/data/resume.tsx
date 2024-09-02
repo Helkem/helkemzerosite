296f9213bb0a4e2758b0cee6d3f0f344632c9e1a
helkem0 <helkem0@protonmail.com>
2024-09-02 17:17:22 +0800

    description updated

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -67,7 +67,7 @@ export const DATA = {
       dates: "2023",
       active: true,
       description:
-        "A Web3 React App to send Stablecoins, view Onchain balances, and check prices against USD. Integrates the use of Wagmi and Viem libraries to interact with the ERC20 smart contracts on ETH Mainnet. Only send real tokens unless intended.",
+        "A Web3 React App to send Stablecoins, View Onchain balances, and check prices against USD. Integrates the use of Wagmi and Viem libraries to interact with the ERC20 smart contracts on ETH Mainnet. Only send real tokens unless intended.",
       technologies: [
         "React",
         "Vite",
